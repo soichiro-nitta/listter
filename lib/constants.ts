2cba85b3676074de0e4afaad47d2782cfc32da8c
soichiro-nitta <soic1226@gmail.com>
2021-01-02 11:58:14 +0900

    update constants

--- lib/constants.ts
+++ lib/constants.ts
@@ -6,8 +6,10 @@ export const TWITTER_ACCESS_TOKEN_KEY =
 export const TWITTER_ACCESS_TOKEN_SECRET =
   process.env.NEXT_PUBLIC_TWITTER_ACCESS_TOKEN_SECRET || ''
 
-export const TWITTER_CONSUMER_KEY =
-  process.env.NEXT_PUBLIC_TWITTER_CONSUMER_KEY || ''
+export const TWITTER_API_KEY = process.env.NEXT_PUBLIC_TWITTER_API_KEY || ''
 
-export const TWITTER_CONSUMER_SECRET =
-  process.env.NEXT_PUBLIC_TWITTER_CONSUMER_SECRET || ''
+export const TWITTER_API_KEY_SECRET =
+  process.env.NEXT_PUBLIC_TWITTER_API_KEY_SECRET || ''
+
+export const TWITTER_CALLBACK_URL =
+  'https://listter.vercel.app/api/auth/callback/twitter'
