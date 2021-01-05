261523031414b2454b6d1536cf8b0f596fc64c5c
soichiro-nitta <soic1226@gmail.com>
2021-01-05 15:22:36 +0900

    wip

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,22 @@
 import { GetServerSideProps } from 'next'
 import { getSession, signIn, signOut, useSession } from 'next-auth/client'
-import { listenerCount } from 'process'
-import useSWR, { mutate } from 'swr'
+import { useEffect, useState } from 'react'
+// import useSwr, { mutate } from 'swr'
 import Twitter from 'twitter'
 
 import {
-  TWITTER_ACCESS_TOKEN_KEY,
-  TWITTER_ACCESS_TOKEN_SECRET,
+  LIST_LIST,
   TWITTER_API_KEY,
   TWITTER_API_KEY_SECRET,
 } from '~/lib/constants'
+import { useGetTimeline } from '~/lib/hooks/useRequest'
+import { List, Timeline } from '~/lib/types'
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
   const session = await getSession(context)
-  let lists = {}
+  let listList = [{}] as List[]
+  const timelineList = [[]] as Timeline[]
+
   if (session) {
     const client = new Twitter({
       access_token_key: session.accessToken,
@@ -21,43 +24,44 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
       consumer_key: TWITTER_API_KEY,
       consumer_secret: TWITTER_API_KEY_SECRET,
     })
-    lists = await client.get('lists/list', {
-      reverse: true,
-    })
-    // res = await client.post('lists/create', {
-    //   description: 'unko2021',
-    //   mode: 'private',
-    //   name: 'unko list',
-    // })
+
+    listList = LIST_LIST
+    // listList = (await client.get('lists/list', {
+    //   reverse: true,
+    // })) as List[]
+
+    // for (const list of listList) {
+    //   const _timeline = (await client.get('lists/statuses', {
+    //     list_id: list.id_str,
+    //   })) as Timeline
+    //   timelineList.push(_timeline)
+    // }
   }
 
   return {
-    props: { lists },
+    props: { listList, timelineList },
   }
 }
 
 const Page: React.FC<{
-  lists: {
-    created_at: string
-    description: string
-    following: boolean
-    full_name: string
-    id: number
-    id_str: string
-    member_count: number
-    mode: string
-    name: string
-    slug: string
-    subscriber_count: number
-    uri: string
-    user: {}
-  }[]
+  listList: List[]
+  // timelineList: Timeline[]
 }> = (props) => {
+  const [currentId, setCurrentId] = useState(props.listList[0].id_str)
   const [session, loading] = useSession()
-  console.log(props.lists)
+  const { timeline } = useGetTimeline(`/api/timeline/${currentId}`)
 
   // mutate('session', session)
-  // const { data } = useSWR('session')
+  // const { data } = useSwr('session')
+
+  const clickList = ({ target }) => {
+    const id = target.getAttribute('data-id')
+    setCurrentId(id)
+  }
+
+  useEffect(() => {
+    console.log({ timeline })
+  }, [timeline])
 
   return (
     <div className="px-8">
@@ -68,9 +72,33 @@ const Page: React.FC<{
             Sign out
           </button>
           <ul className="mt-8">
-            {props.lists.map((list) => (
-              <li key={list.id} className="mt-4">
+            {props.listList.map((list) => (
+              <li
+                key={list.id_str}
+                className="mt-4"
+                data-id={list.id_str}
+                onClick={clickList}
+              >
                 {list.name}
+              </li>
+            ))}
+          </ul>
+          <ul className="mt-8">
+            {timeline?.map((tweet) => (
+              <li key={tweet.id_str} className="flex justify-between">
+                <div className="w-16 h-16">
+                  <img
+                    src={tweet.user.profile_image_url}
+                    className="object-cover w-full h-full rounded-full"
+                  />
+                </div>
+
+                <div className="w-92 font-inter">
+                  <div className="font-bold">{tweet.user.name}</div>
+                  <div className="">{tweet.user.screen_name}</div>
+                  <div className="">{tweet.created_at}</div>
+                  <div className="">{tweet.text}</div>
+                </div>
               </li>
             ))}
           </ul>
